df9dc8ea73814d3fbf1977bb98920dfe28a14ecf
Ankit yadav <ay870421@gmail.com>
2025-07-23 22:53:45 +0530

    song model done again

--- backend/src/models/postModel.js
+++ backend/src/models/postModel.js
@@ -1,15 +1,18 @@
-
+// models/Post.js
 import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  text: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
 const postSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     image: { type: String, required: true },
     caption: { type: String, default: "" },
-    music: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Music",
-      default: null,
-    },
+    music: { type: mongoose.Schema.Types.ObjectId, ref: "Music", default: null }, // ✅ music ref
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     comments: [commentSchema],
     sharedFrom: {
