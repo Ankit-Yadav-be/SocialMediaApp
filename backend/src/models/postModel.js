b4d76b1366ed7cd4ab165c1a1973a2d0ea022cd2
Ankit yadav <ay870421@gmail.com>
2025-07-23 22:49:50 +0530

    song model done

--- backend/src/models/postModel.js
+++ backend/src/models/postModel.js
@@ -1,3 +1,5 @@
+
+import mongoose from "mongoose";
 const postSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
@@ -19,3 +21,5 @@ const postSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+export default mongoose.model("Post", postSchema);
